7b55cdbb5a5aa07e7d0ca03970c21ac286c36f82
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2019-05-08 13:38:54 -0300

    Defaults to scraping all listed symbols.

--- data_scraper/cboe.py
+++ data_scraper/cboe.py
@@ -1,15 +1,18 @@
 import logging
+import os
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 from .utils import save_data
 
 url = "http://www.cboe.com/delayedquote/quote-table-download"
 
-symbols = ["SPX", "SPY"]
 
-
-def fetch_data(symbols=symbols):
+def fetch_data(symbols=None):
     """Fetches options data for a given list of symbols"""
+
+    symbols = symbols or _get_all_listed_symbols()
+
     logger = logging.getLogger(__name__)
     try:
         form_data = _get_form_data()
@@ -36,6 +39,17 @@ def fetch_data(symbols=symbols):
         except Exception:
             logger.error(
                 "Error fetching symbol %s data", symbol, exc_info=True)
+
+
+def _get_all_listed_symbols():
+    """Returns array of all listed symbols.
+    http://www.cboe.com/publish/scheduledtask/mktdata/cboesymboldir2.csv
+    """
+    current_dir = os.path.join(os.getcwd(), os.path.dirname(__file__))
+    symbols_file = os.path.realpath(
+        os.path.join(current_dir, "cboesymboldir2.csv"))
+    symbols_df = pd.read_csv(symbols_file, skiprows=1)
+    return symbols_df["Stock Symbol"].array
 
 
 def _get_form_data():
