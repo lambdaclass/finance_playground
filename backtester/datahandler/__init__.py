3f96b4b12816608b39feda4a7337c3ffa86c5eea
Juan Pablo Amoroso <juanpablo.amoroso@gmail.com>
2019-05-16 16:11:23 -0300

    Removed backtester, data scraper and alert system

--- backtester/datahandler/__init__.py
+++
@@ -1,4 +0,0 @@
-from .datahandler import DataHandler
-from .historic_datahandler import HistoricDataHandler
-from .spx_datahandler import SPXDataHandler
-from .balanced_datahandler import BalancedDataHandler
